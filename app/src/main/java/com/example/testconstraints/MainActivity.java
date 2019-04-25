ec13f5ff6fe2fa6a98fcaff4c6fc1d24770a67c8
Michał Mokrzycki <michal@mokrzycki.eu>
2019-04-25 12:00:11 +0200

    implemented external library for rotating viewgroup's children

--- app/src/main/java/com/example/testconstraints/MainActivity.java
+++ app/src/main/java/com/example/testconstraints/MainActivity.java
@@ -1,70 +1,79 @@
 package com.example.testconstraints;
 
-import android.support.constraint.ConstraintLayout;
-import android.support.constraint.ConstraintSet;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import com.github.rongi.rotate_layout.layout.RotateLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     ConstraintLayout root;
-    ConstraintSet constraintSet = new ConstraintSet();
-    LinearLayout container;
+    RotateLayout container;
     Button button1, button2, button3, button4;
-    TextView textView, textCoord;
-    private ViewGroup.LayoutParams layoutParams;
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        button1 = findViewById(R.id.button);
-        button2 = findViewById(R.id.button2);
-        button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
         root = findViewById(R.id.root);
+
+        button1 = findViewById(R.id.button);
+        button1.setText("+90");
+
+        button2 = findViewById(R.id.button2);
+        button2.setText("+180");
+
+        button3 = findViewById(R.id.button3);
+        button3.setText("-90");
+
+        button4 = findViewById(R.id.button4);
+        button4.setText("+360");
+
         textView = findViewById(R.id.text);
-        layoutParams = textView.getLayoutParams();
-        textCoord = findViewById(R.id.textCoord);
-        constraintSet.clone(root);
+
         container = findViewById(R.id.container);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int containerId = container.getId();
-                //container.setRotation(container.getRotation() + 90);
-                textView.setRotation(textView.getRotation() + 90);
-                constraintSet.clear(containerId);
-                constraintSet.connect(containerId, ConstraintSet.START, button4.getId(), ConstraintSet.END, 8);
-                constraintSet.connect(containerId, ConstraintSet.TOP, button1.getId(), ConstraintSet.BOTTOM, 8);
-                constraintSet.connect(containerId, ConstraintSet.END, button3.getId(), ConstraintSet.START, 8);
-                constraintSet.connect(containerId, ConstraintSet.BOTTOM, button2.getId(), ConstraintSet.TOP, 8);
-                constraintSet.constrainWidth(containerId, ConstraintSet.MATCH_CONSTRAINT);
-                constraintSet.constrainHeight(containerId, ConstraintSet.MATCH_CONSTRAINT);
-                constraintSet.constrainWidth(textView.getId(), ConstraintSet.MATCH_CONSTRAINT);
-                constraintSet.constrainHeight(textView.getId(), ConstraintSet.MATCH_CONSTRAINT);
-                constraintSet.applyTo(root);
+                container.setAngle(90);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                container.setAngle(180);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                container.setAngle(-90);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                container.setAngle(0);
             }
         });
     }
 
-
     @Override
     protected void onResume() {
         super.onResume();
-        //show();
-
     }
 
-//    public void show() {
-//        textCoord.setText("(" + textView.getLeft() + "," + textView.getTop() + ") size w = " + textView.getWidth() + " h = " + textView.getHeight());
-//    }
+
 }
 
